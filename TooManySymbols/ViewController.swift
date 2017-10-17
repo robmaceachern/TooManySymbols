e483bbe2b0a46d29334a26d543a1d629c773505f
Rob MacEachern <rob@robmaceachern.com>
2017-10-17 11:37:16 -0700

    Sample project init

---
+++ TooManySymbols/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TooManySymbols
+//
+//  Created by Robert MacEachern on 2017-10-17.
+//  Copyright © 2017 Steamclock Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
